bf26ff8e39f8946a91badaa9980921bda08ca3a2
lucwii <milanoviclukaa23@gmail.com>
2026-01-07 19:21:27 +0100

    added the sructure and supabase logic

---
+++ src/app/services/gallery.service.ts
@@ -0,0 +1,17 @@
+import { supabaseClient } from "../lib/supabase/client";
+import { GalleryImage } from "../types/gallery";
+
+export const getGalleryImages = async (): Promise<GalleryImage[]> => {
+    const {data, error} = await supabaseClient
+        .from("gallery_images")
+        .select("*")
+        .eq('published', true)
+        .order('created_at', {ascending: false})
+
+    if(error) {
+        console.error(error);
+        return [];
+    }
+
+    return data || [];
+}
---
+++ src/app/types/gallery.ts
@@ -0,0 +1,7 @@
+export type GalleryImage = {
+    image_url: string,
+    id: string,
+    title: string,
+    description: string,
+    created_at: string
+}
